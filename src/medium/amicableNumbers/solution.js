80d35124f3e3f5024ffd268224046bf7a097eedf
Oliver West <oliversimon.west@gmail.com>
2022-11-22 18:19:39 +0000

    changed amicable numbers to be faster

--- src/medium/amicableNumbers/solution.js
+++ src/medium/amicableNumbers/solution.js
@@ -12,19 +12,19 @@ const allAmicableNumbers = (num) => {
     // let results = ``
     let resultArray = []
     for (let i = 1; i <= num; i++) {
-        // let firstNumber = sumOfProperDivisors(i)
-        // let secondNumber = sumOfProperDivisors(sumOfProperDivisors(i))
+        let firstNumber = sumOfProperDivisors(i)
+        let secondNumber = sumOfProperDivisors(sumOfProperDivisors(i))
 
-        // if (secondNumber === i && firstNumber !== i) {
+        if (secondNumber === i && firstNumber !== i) {
 
         // Changed this step away from the variables, which seemed to make the code work a little bit faster. Harder to read for humans though.
         
-        if (sumOfProperDivisors(sumOfProperDivisors(i)) === i && sumOfProperDivisors(i) !== i) {
+        // if (sumOfProperDivisors(sumOfProperDivisors(i)) === i && sumOfProperDivisors(i) !== i) {
             
             // results = results + ` ${sumOfProperDivisors(i)} and ${sumOfProperDivisors(sumOfProperDivisors(i))} are amicable pairs.`
 
             // Changed the results to be in an array, for easier reading, and possibly performance.
-            resultArray.push(`${sumOfProperDivisors(i)} and ${sumOfProperDivisors(sumOfProperDivisors(i))} are amicable pairs.`)
+            resultArray.push(`${firstNumber} and ${secondNumber} are amicable pairs.`)
         }
     }
     // return results
@@ -36,5 +36,5 @@ const allAmicableNumbers = (num) => {
 // console.log('i = firstnumber (284), second number =', sumOfProperDivisors(284))
 // console.log(sumOfProperDivisors(sumOfProperDivisors(100)))
 
-console.log(allAmicableNumbers(10000))
+console.log(allAmicableNumbers(100000))
 
